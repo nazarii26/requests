64a981df621b20e99caed2458284de908eca764a
Nazarii Pirih <nazaryipyryg@gmail.com>
2022-05-08 12:12:21 +0300

    added console.log

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -6,6 +6,8 @@ export const Delete = () => {
 		fetch('https://jsonplaceholder.typicode.com/posts/1', {
 			method: 'DELETE',
 		})
+			.then(response => response.json())
+			.then(json => console.log('DELETED', json));
 	})
 	return (
 		<h1>DELETE</h1>
